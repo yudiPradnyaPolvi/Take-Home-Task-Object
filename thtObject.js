87ba16dc83a654f881ff308ed6ea342b93178442
yudiPradnyaPolvi <pradnyakemenuh@gmail.com>
2023-03-12 08:47:59 +0800

    first commit

---
+++ thtObject.js
@@ -0,0 +1,124 @@
+console.log ("Take Home Task Object")
+
+console.log ("Data Karyawan PT. Maju Abadi")
+const dataKaryawan = {
+    nama : "Bambang Suprianto",
+    ttl : "Singaraja, 17 April 2023",
+    umur : 20 ,
+    jumlahSaudara : 3,
+    hobi:["bola","bulutangkis"],
+    
+    objectWisataFav : objectWisataFavs = {
+                    Bali : "Gwk",
+                    jakarta : "monas",
+                    bandung: "kebun teh",
+                    Medan : "danau Toba",
+                    jogja :"malioboro"
+
+    }   
+
+
+}
+console.log (dataKaryawan.nama)
+console.log (dataKaryawan.ttl)
+console.log (dataKaryawan.umur)
+console.log (dataKaryawan.jumlahSaudara)
+console.log (dataKaryawan.hobi)
+console.log (dataKaryawan.objectWisataFav)
+
+
+console.log ("Data Mahasiswa Berprestasi")
+const dataMapres = {
+    namaMahasiswa : "Adel",
+    nim : 2005541111,
+    jurusan : "Kesmas",
+    jumlahPrestasi : 5,
+    jenisPrestasi :["DIKTI","Nasional Mandiri","Regional Mandiri"],
+    
+    keterangan : keteranganPrestasi = {
+                    Dikti: "Silver Medal Pimnas",
+                    nasionalMandiri: "juara 1 Lomba Esai ITERA",
+                    regionalMandiri: "Gold Medal INNOPA WINTEX IID 2022"
+
+    }   
+
+
+}
+console.log (dataMapres.namaMahasiswa)
+console.log (dataMapres.nim)
+console.log (dataMapres.jurusan)
+console.log (dataMapres.jumlahPrestasi)
+console.log (dataMapres.jenisPrestasi)
+console.log (dataMapres.keterangan)
+
+console.log ("Data Sekolah SMA Negeri 1 Kuta")
+const dataSekolah = {
+    namaSekolah : "SMA Negeri 1 Kuta",
+    alamatSekolah : "Jalan Dewi Saraswati",
+    tahunBerdiri : 2013,
+    jumlahSiswa : 300,
+    jurusan :["IPA","IPS","Bahasa"],
+    
+    jumlahGuru : jumlahGuru1 = {
+                    IPA: 33,
+                    IPS: 15 ,
+                    Bahasa: 10 
+
+    }   
+
+
+}
+console.log (dataSekolah.namaSekolah)
+console.log (dataSekolah.alamatSekolah)
+console.log (dataSekolah.tahunBerdiri)
+console.log (dataSekolah.jumlahSiswa)
+console.log (dataSekolah.jurusan)
+console.log (dataSekolah.jumlahGuru)
+
+console.log ("Data UMKM di Provinsi Bali")
+const dataUMKM = {
+    namaUMKM: "PT. Indah Permata Maju",
+    alamatUMKM : "Jalan Kampus Sudirman",
+    tahunBerdiri : 2006,
+    jumlahPemasukanHarian : 1000000,
+    jenisJualan :["Emas","Perak","Perunggu"],
+    
+    jumlahCabang: cabang = {
+                    Jimbaran: 5,
+                    Kuta: 3 ,
+                    Denpasar: 4 
+
+    }   
+
+
+}
+console.log (dataUMKM.namaUMKM)
+console.log (dataUMKM.alamatUMKM)
+console.log (dataUMKM.tahunBerdiri)
+console.log (dataUMKM.jumlahPemasukanHarian)
+console.log (dataUMKM.jenisJualan)
+console.log (dataUMKM.jumlahCabang)
+
+console.log ("Data Penerima Beasiswa Bidikmisi Universitas Udayana 2023")
+const dataAwardee= {
+    namaMahasiswa: "PT. Indah Permata Maju",
+    nim : 2006431231,
+    alamatMahasiswa : "Jalan Kampus Sudirman",
+    jumlahDanaBantuan : 5000000,
+    jenisBantuan:["Ukt","Makan","Transportasi"],
+    
+    detailBantuan: detail = {
+                    Ukt: 5000000,
+                    makanan: 3000000 ,
+                    transporasi: 200000
+
+    }   
+
+
+}
+console.log (dataAwardee.namaMahasiswa)
+console.log (dataAwardee.nim)
+console.log (dataAwardee.alamatMahasiswa)
+console.log (dataAwardee.jumlahDanaBantuan)
+console.log (dataAwardee.jenisBantuan)
+console.log (dataAwardee.detailBantuan)
